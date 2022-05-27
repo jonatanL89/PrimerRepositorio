07c334fb96b2759111b5ec5de3b1293ae5828b9c
jonatanL89 <105823772+jonatanL89@users.noreply.github.com>
2022-05-27 16:54:43 -0300

    Delete ejercicio5.py

--- ejercicio5.py
+++
@@ -1,4 +0,0 @@
-horastrabajadas = int(input("Ingrese el numero de horas trabajadas: "))
-costoxhora = int(input("Ingrese el valor de la hora trabajada: "))
-
-print ("El valor total x sus dias trabajados es de: ",horastrabajadas*costoxhora, "Pesos argentinos")
