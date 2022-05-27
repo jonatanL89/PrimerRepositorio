75593a5683ae40255cacdc039e9db1d913f757d3
jonatanL89 <105823772+jonatanL89@users.noreply.github.com>
2022-05-27 16:53:33 -0300

    Delete ejercicio2-1.py

--- ejercicio2-1.py
+++
@@ -1,7 +0,0 @@
-capital = int(input("Ingrese el capital inicial: "))
-interes = int(input("Ingrese la tasa de interes: "))
-anios = int(input("Ingrese el numero de años: "))
-
-Cn = (capital * (1 + interes/100) ** anios)
-
-print (Cn)
